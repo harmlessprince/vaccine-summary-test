dac873d553494155ff2b22818b84670922a1bd5e
harmlessprince <realolamilekan@gmail>
2022-08-09 12:39:04 +0100

    mogo db setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true})],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.DATABASE_URL,  { useNewUrlParser: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+// , {
+//   auth: {
+//     username: process.env.DATABASE_USERNAME,
+//     password: process.env.DATABASE_PASSWORD,
+//   },
+//   dbName: process.env.DATABASE_NAME
+// }
