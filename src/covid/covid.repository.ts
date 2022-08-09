76a5f8aa91f4b78655c34ba85bddae7abe68521e
harmlessprince <realolamilekan@gmail>
2022-08-09 17:07:57 +0100

    Covid model repository initiated

---
+++ src/covid/covid.repository.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Covid } from './covid';
+
+@Injectable()
+export class CovidRepository {
+    constructor(@InjectModel('Covid') private readonly covidModel: Model<Covid>){}
+
+}
