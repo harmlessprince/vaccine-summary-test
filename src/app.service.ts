3d98668c86ad4476257be348625fcfb3d9193fff
harmlessprince <realolamilekan@gmail>
2022-08-09 16:39:07 +0100

    csv file is now been read

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,10 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import * as fs from 'fs';
 import { CsvParser } from 'nest-csv-parser';
-
 @Injectable()
 export class AppService {
-  constructor(private readonly scvParser: CsvParser){}
+  constructor(private readonly csvParserService: CsvParser){}
   getHello(): string {
     return 'Hello World!';
   }
@@ -13,10 +12,33 @@ export class AppService {
   async seedDatabase(){
     // console.log(__dirname + 'data.csv');
     const dataFilePath = __dirname + '/data/data.csv';
-    console.log(fs.existsSync(dataFilePath));
-//     const stream = fs.createReadStream(dataFilePath);
-// console.log(stream);
-    //  const response = 
-    //  console.log(response.data)
+    console.log();
+    if (fs.existsSync(dataFilePath)) {
+      const stream = fs.createReadStream(dataFilePath, { encoding: "utf-8" });
+      const VaccineSummaryEntities = await this.csvParserService.parse(stream, VaccineSummaryEntity)
+      console.log(VaccineSummaryEntities);
+    }else{
+      throw new NotFoundException('Csv file not found')
+    }
+    
+
   }
+}
+
+class VaccineSummaryEntity {
+  YearWeekISO: string
+  FirstDose: number
+  FirstDoseRefused: string
+  SecondDose: number
+  DoseAdditional1: number
+  DoseAdditional2: number
+  UnknownDose: number
+  NumberDosesReceived: number
+  NumberDosesExported: number
+  Region: string
+  Population: number
+  ReportingCountry: string
+  TargetGroup: string
+  Vaccine: string
+  Denominator: number
 }
