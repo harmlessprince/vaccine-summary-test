8423d9bebeaf5671d1cc9a206688942c3225d8f5
harmlessprince <realolamilekan@gmail>
2022-08-13 02:18:36 +0100

    nest now used enviroment variable port

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT;
+  await app.listen(port);
 }
 bootstrap();
